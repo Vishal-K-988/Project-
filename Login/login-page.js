1bff6cf89df4d295c25a1b7a279016d827bb143c
Vishal.K <vishalkumarvkvk988@gmail.com>
2022-05-11 04:01:41 +0530

    Created about page

--- Login/login-page.js
+++ Login/login-page.js
@@ -10,27 +10,32 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.email.value;
     const password = loginForm.password.value;
 
-    if (username === "vishalkumarvkvk988@gmail.com" && password === "123") {
+    if (username === "vishalkumarvkvk988@gmail.com" && password === "Hardworker") {
         alert("You have successfully logged in.");
         // location.reload();
         window.location.href = "webpage.html";
     } 
-    else if(username === "mohit@gmail.com" && password === "123") {
+    else if(username === "mohit@gmail.com" && password === "Chocolate") {
         alert("You have successfully logged in.");
         // location.reload();
         window.location.href = "webpage.html";
     }
 
-    else if(username === "sudhanshu@gmail.com" && password === "123") {
+    else if(username === "sudhanshu@gmail.com" && password === "Designer") {
     alert("You have successfully logged in.");
     // location.reload();
     window.location.href = "webpage.html";
     }
-    else if(username === "hardik@gmail.com" && password === "123") {
+    else if(username === "hardik@gmail.com" && password === "Resources") {
         alert("You have successfully logged in.");
         // location.reload();
         window.location.href = "webpage.html";
         }
+        else if(username === "chandan@gmail.com" && password === "loafing") {
+            alert("You have successfully logged in.");
+            // location.reload();
+            window.location.href = "webpage.html";
+            }
     else {
         alert("error.");
     }
